763d9beb78f185d622ed8c8932ae817abf85d2cf
sansusan <sansusan@yandex.ru>
2015-05-25 13:38:07 +0300

    add theme, globalization, extensions

--- JqwidgetsAsset.php
+++ JqwidgetsAsset.php
@@ -12,28 +12,75 @@ class JqwidgetsAsset extends AssetBundle
     public $css = [
     ];
     public $js = [
-        'jqx-all.js',
+        'jqxcore.js',
     ];
     public $depends = [
         'yii\web\JqueryAsset',
     ];
+
+    public static $theme = 'base';
+
+    public static $globalize = 'ru-RU';
+
+    public static $extensions = [];
 
     /**
      * @inheritdoc
      */
     public function init()
     {
+        $this->applyTheme();
+        $this->applyGlobalize();
+        $this->applyExtensions();
         parent::init();
+    }
+
+
+    protected function applyTheme()
+    {
+        if (!empty(self::$theme) && is_string(self::$theme))
+            $theme = self::$theme;
+        else
+            $theme = 'base';
+        array_unshift($this->css, strtr('styles/jqx.{theme}.css', ['{theme}' => $theme]));
+    }
+
+
+    protected function applyGlobalize()
+    {
+        if (!empty(self::$globalize) && is_string(self::$globalize))
+            $globalize = self::$globalize;
+        else
+            $globalize = 'ru-RU';
+
+        array_push($this->js, 'globalization/globalize.js');
+        array_push($this->js, strtr('globalization/globalize.culture.{globalize}.js', ['{globalize}' => $globalize]));
+    }
+
+    protected function applyExtensions()
+    {
+        foreach (self::$extensions as $ex) {
+            if (is_string($ex)) {
+                    array_push($this->js, strtr('{ex}.js', ['{ex}' => $ex]));
+            }
+        }
     }
 
     /**
      * @param \yii\web\View $view
+     * @param array $options
+     * @param array $extensions
      * @return static
      */
-    public static function register($view)
+    public static function register($view,  $options = [], $extensions = [])
     {
+        if (is_array($options)) {
+            if (array_key_exists('theme', $options)) self::$theme = $options['theme'];
+            if (array_key_exists('globalize', $options)) self::$globalize = $options['globalize'];
+        }
+
+        if (is_array($extensions) && !empty($extensions)) self::$extensions = $extensions;
         return parent::register($view);
     }
-
 
 }
